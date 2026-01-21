85c5f90a2a617ca5c6e2a7f528bea7a0ba7e4b91
Christoph Steinbeck <christoph.steinbeck@uni-jena.de>
2026-01-21 14:19:54 +0100

    feat(07-03): export geometry functions from nwchem package
    - Add smiles_to_xyz, load_geometry_file, mol_to_xyz_block exports
    - Add validate_geometry export
    - Organize exports by category (geometry, input generation)

--- src/qm_nmr_calc/nwchem/__init__.py
+++ src/qm_nmr_calc/nwchem/__init__.py
@@ -5,12 +5,24 @@ and NMR shielding calculations with COSMO solvation, replacing the previous
 ISiCLE dependency.
 """
 
+from qm_nmr_calc.nwchem.geometry import (
+    load_geometry_file,
+    mol_to_xyz_block,
+    smiles_to_xyz,
+    validate_geometry,
+)
 from qm_nmr_calc.nwchem.input_gen import (
     generate_optimization_input,
     generate_shielding_input,
 )
 
 __all__ = [
+    # Geometry handling
+    "smiles_to_xyz",
+    "load_geometry_file",
+    "mol_to_xyz_block",
+    "validate_geometry",
+    # Input generation
     "generate_optimization_input",
     "generate_shielding_input",
 ]
