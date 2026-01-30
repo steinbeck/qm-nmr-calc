1fbf48c8828c0690cbac32c5a18e3c95c9db7404
Christoph Steinbeck <christoph.steinbeck@uni-jena.de>
2026-01-30 09:57:08 +0100

    feat(24-03): export clustering and xTB ranking functions from conformers module
    - Add clustering module exports: cluster_and_select, cluster_conformers_by_rmsd, select_cluster_representatives
    - Add xTB ranking module exports: detect_xtb_available, get_xtb_version, rank_conformers_by_xtb
    - Update __all__ list with alphabetical ordering

--- src/qm_nmr_calc/conformers/__init__.py
+++ src/qm_nmr_calc/conformers/__init__.py
@@ -5,14 +5,30 @@ from .boltzmann import (
     average_nmr_shifts,
     calculate_boltzmann_weights,
 )
+from .clustering import (
+    cluster_and_select,
+    cluster_conformers_by_rmsd,
+    select_cluster_representatives,
+)
 from .crest_generator import detect_crest_available, generate_crest_ensemble
 from .pipeline import generate_conformer_ensemble
+from .xtb_ranking import (
+    detect_xtb_available,
+    get_xtb_version,
+    rank_conformers_by_xtb,
+)
 
 __all__ = [
     "average_ensemble_nmr",
     "average_nmr_shifts",
     "calculate_boltzmann_weights",
+    "cluster_and_select",
+    "cluster_conformers_by_rmsd",
     "detect_crest_available",
+    "detect_xtb_available",
     "generate_conformer_ensemble",
     "generate_crest_ensemble",
+    "get_xtb_version",
+    "rank_conformers_by_xtb",
+    "select_cluster_representatives",
 ]
